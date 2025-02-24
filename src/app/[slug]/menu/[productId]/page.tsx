a4f6351563559794d22497fa6dc7d4b05fb7595b
rogerio.jlf <rogerio.jlf@gmail.com>
2025-02-24 09:04:31 -0300

    feat: add product header components

---
+++ src/app/[slug]/menu/[productId]/page.tsx
@@ -0,0 +1,35 @@
+import { notFound } from "next/navigation";
+
+import { dbPrisma } from "@/lib/prisma";
+
+import ProductHeader from "./components/product-header";
+
+interface ProductPageProps {
+  params: Promise<{slug: string, productId: string}>;
+}
+
+const ProductPage = async ({params}: ProductPageProps) => {
+  const {slug, productId} = await params;
+  const product = await dbPrisma.product.findUnique({
+    where:{
+      id: productId
+    }
+  })
+
+  if(!product) {
+    return notFound();
+  }
+  
+  return (
+    <>
+    <ProductHeader product={product} />
+    <h1>
+      Product Page
+    </h1>
+      {slug}
+      {productId}
+    </>
+  );
+}
+
+export default ProductPage;
