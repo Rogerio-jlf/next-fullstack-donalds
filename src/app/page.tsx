ddfed507b7d1ff6f788a0621e575b51ad5b50a43
rogerio.jlf <rogerio.jlf@gmail.com>
2025-03-03 17:42:40 -0300

    feat: new update files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,22 @@
+"use client";
+
+import { useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+
 const HomePage = () => {
-  return <h1>hello world</h1>;
+  const router = useRouter();
+  const handleClick = () => {
+    router.push("/fsw-donalds");
+  };
+
+  return (
+    <>
+      <div>
+        <h1>hello world</h1>;<Button onClick={handleClick}>Click me</Button>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
